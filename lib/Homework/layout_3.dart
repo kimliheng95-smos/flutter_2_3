c8bf7f53997bf6d2698d3c43935832696aa6c0ef
hengmengleap <leapkmeng@gmal.com>
2026-06-26 12:15:22 +0700

    Layout3 Added

---
+++ lib/Homework/layout_3.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Layout3 extends StatefulWidget {
+  const Layout3({super.key});
+
+  @override
+  State<Layout3> createState() => _Layout3State();
+}
+
+class _Layout3State extends State<Layout3> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
